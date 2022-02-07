c3354f5ab7b1bca3c2386947ad6add8d4df7556c
Mot0511 <suvorov.matvej9@gmail.com>
2022-02-07 11:02:47 +0300

    _

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="ru" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>ShawClub</title>
@@ -13,14 +13,49 @@
         background: #3F1A00;
         border-radius: 20px;
         width: 297px;
-        height: 349px;
+        padding-bottom: 20px;
         margin-top: 25px;
       }
       .heading{
         font-size: 36px;
         color: white;
       }
-
+      .orderNow{
+        width: 134px;
+        height: 55px;
+        border: 5px solid #CE0000;
+        border-radius: 20px;
+        background: none;
+        color: white;
+        font-size: 18px;
+        transition: background-color 0.3s;
+      }
+      .orderNow:hover{
+        background-color: #CE0000;
+      }
+      .addToCart{
+        width: 134px;
+        height: 55px;
+        background: #CE0000;
+        border-radius: 20px;
+        border: 0px;
+        margin-left: 10px;
+        margin-top: 10px;
+        color: white;
+        font-size: 20px;
+        transition: background-color 0.2s;
+      }
+      .addToCart:hover{
+        background-color: #870000;
+      }
+      .imageProduct{
+        margin-top: 20px;
+        width: 80%;
+        border-radius: 20px;
+      }
+      .product p{
+        font-size: 20px;
+      }
     </style>
   </head>
   <body>
@@ -31,17 +66,38 @@
         <div class="row">
           <div class="col-lg-4">
             <div class="product">
-
+              <center>
+                <div class="orderBts">
+                <img src="img/products/_1.jpg" class="imageProduct" alt="">
+                <p>Классическая</p>
+                <button type="button" class="orderNow" name="button">Сразу купить</button>
+                <button type="button" class="addToCart" name="button">В корзину</button>
+              </div>
+            </center>
             </div>
           </div>
           <div class="col-lg-4">
             <div class="product">
-
+              <center>
+              <img src="img/products/_2.jpg" class="imageProduct" alt="">
+              <p>Сырная</p>
+              <div class="orderBts">
+              <button type="button" class="orderNow" name="button">Сразу купить</button>
+              <button type="button" class="addToCart" name="button">В корзину</button>
+            </div>
+              </center>
             </div>
           </div>
           <div class="col-lg-4">
             <div class="product">
-
+              <center>
+              <img src="img/products/_3.jpg" class="imageProduct" alt="">
+              <p>Домашняя</p>
+              <div class="orderBts">
+              <button type="button" class="orderNow" name="button">Сразу купить</button>
+              <button type="button" class="addToCart" name="button">В корзину</button>
+            </div>
+              </center>
             </div>
           </div>
         </div>
@@ -54,19 +110,29 @@
         <div class="row">
           <div class="col-lg-4">
             <div class="product">
-
+              <center>
+                <div class="orderBts">
+                <img src="img/products/4.jpg" class="imageProduct" alt="">
+                <p>Кофе "Американо"</p>
+                <button type="button" class="orderNow" name="button">Сразу купить</button>
+                <button type="button" class="addToCart" name="button">В корзину</button>
+              </div>
+            </center>
             </div>
           </div>
           <div class="col-lg-4">
             <div class="product">
-
+              <center>
+              <img src="img/products/5.jpg" class="imageProduct" alt="">
+              <p>Чай</p>
+              <div class="orderBts">
+              <button type="button" class="orderNow" name="button">Сразу купить</button>
+              <button type="button" class="addToCart" name="button">В корзину</button>
+            </div>
+              </center>
             </div>
           </div>
-          <div class="col-lg-4">
-            <div class="product">
 
-            </div>
-          </div>
         </div>
 
       </div>
